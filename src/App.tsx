5d49dc45904378a3c5dc7687188cd722853172c7
moewaizzz <moewai3127@gmail.com>
2023-12-02 15:53:47 +0630

    charkra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
+import { Button } from "@chakra-ui/react";
 
 function App() {
- 
-
   return (
-    <> <p>hello</p>
+    <>
+      <Button colorScheme="blue">Button</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
