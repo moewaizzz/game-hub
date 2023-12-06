78b64d6021a669c3e9d4f39d2c9bc1b98c8bc627
moewaizzz <moewai3127@gmail.com>
2023-12-07 01:28:09 +0630

    Sort the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "0bbf828760164d16911c1bd4c3c907af",
+    key: "033b48a63821470abf6d437a75b31111",
   },
 });
